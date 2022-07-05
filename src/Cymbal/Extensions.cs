8b2695d6a1bc59c36a45ea20a4a89c635618979c
Simon Cropp <simon.cropp@gmail.com>
2022-07-05 13:43:39 +1000

    Update Extensions.cs

--- src/Cymbal/Extensions.cs
+++ src/Cymbal/Extensions.cs
@@ -3,7 +3,9 @@ static class Extensions
     //To work around https://github.com/dotnet/runtime/issues/27128
     public static bool DoubleWaitForExit(this Process process)
     {
-        var result = process.WaitForExit(60000);
+        //4min30sec
+        var timeout = 270000;
+        var result = process.WaitForExit(timeout);
         if (result)
         {
             process.WaitForExit();
